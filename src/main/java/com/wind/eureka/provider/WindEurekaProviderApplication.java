92be046f4763065a93b2d4d18a7675845466c940
qianchun <qianchun@huli.com>
2017-10-25 15:00:29 +0800

    init

--- src/main/java/com/wind/eureka/provider/WindEurekaProviderApplication.java
+++ src/main/java/com/wind/eureka/provider/WindEurekaProviderApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 
-@Import({ DispatcherServletAutoConfiguration.class, EmbeddedServletContainerAutoConfiguration.class,
-		ErrorMvcAutoConfiguration.class, HttpEncodingAutoConfiguration.class,
-		HttpMessageConvertersAutoConfiguration.class, JacksonAutoConfiguration.class, MultipartAutoConfiguration.class,
-		ServerPropertiesAutoConfiguration.class, WebMvcAutoConfiguration.class })
+//@Import({ DispatcherServletAutoConfiguration.class, EmbeddedServletContainerAutoConfiguration.class,
+//		ErrorMvcAutoConfiguration.class, HttpEncodingAutoConfiguration.class,
+//		HttpMessageConvertersAutoConfiguration.class, JacksonAutoConfiguration.class, MultipartAutoConfiguration.class,
+//		ServerPropertiesAutoConfiguration.class, WebMvcAutoConfiguration.class })
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.wind")
 public class WindEurekaProviderApplication {
